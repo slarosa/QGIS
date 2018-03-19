3f8e4aa58a4e21504200ff1c81099fd27ef815c4
Salvatore Larosa <lrssvtml@gmail.com>
2018-03-19 23:16:03 +0100

    add functon to get magnification value

--- src/app/qgsstatusbarmagnifierwidget.cpp
+++ src/app/qgsstatusbarmagnifierwidget.cpp
@@ -86,3 +86,8 @@ void QgsStatusBarMagnifierWidget::setMagnification( double value )
 {
   emit magnificationChanged( value / 100 );
 }
+
+int QgsStatusBarMagnifierWidget::magnification()
+{
+  mSpinBox->value();
+}
